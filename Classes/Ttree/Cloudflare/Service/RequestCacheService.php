ab1541bd4cfc26d59900e0542e4674fd2fd6aba1
Dominique Feyer <dfeyer@ttree.ch>
2013-12-06 19:38:30 +0100

    [BUGFIX] Cache is rebuild on every request

--- Classes/Ttree/Cloudflare/Service/RequestCacheService.php
+++ Classes/Ttree/Cloudflare/Service/RequestCacheService.php
@@ -12,9 +12,12 @@ namespace Ttree\Cloudflare\Service;
  *                                                                        */
 
 use Ttree\Cloudflare\Browser;
+use Ttree\Cloudflare\CacheDefinition;
+use Ttree\Cloudflare\Factory\CacheDefinitionFactory;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Cache\Frontend\StringFrontend;
 use TYPO3\Flow\Log\SystemLoggerInterface;
+use TYPO3\Flow\Utility\Arrays;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 
 /**
@@ -29,9 +32,21 @@ class RequestCacheService {
 
 	/**
 	 * @Flow\Inject
+	 * @var ApiService
+	 */
+	protected $apiService;
+
+	/**
+	 * @Flow\Inject
 	 * @var SystemLoggerInterface
 	 */
 	protected $systemLogger;
+
+	/**
+	 * @Flow\Inject
+	 * @var CacheDefinitionFactory
+	 */
+	protected $cacheDefinitionFactory;
 
 	/**
 	 * @param string $uri
@@ -54,19 +69,30 @@ class RequestCacheService {
 	/**
 	 * @param string $uri
 	 * @param array $tags
+	 * @param CacheDefinition $cacheDefinition
 	 * @return void
 	 */
-	public function set($uri, array $tags = array()) {
+	public function set($uri, array $tags = array(), CacheDefinition $cacheDefinition) {
+		if ($cacheDefinition->getEnable() === FALSE) {
+			return;
+		}
 		$entryIdentifier = $this->createRequestUriCacheRecordIdentifier($uri);
-		$this->cache->set($entryIdentifier, $uri, $tags, 0);
+		$value = $uri . '||' . $cacheDefinition->getZone();
+		$this->cache->set($entryIdentifier, $value, $tags, 0);
 	}
 
 	/**
 	 * @param NodeInterface $node
 	 */
 	public function purgeCacheByNode(NodeInterface $node) {
-		foreach ($this->cache->getByTag($node->getIdentifier()) as $uri) {
-			$this->purgeCacheByRequestUri($uri);
+		foreach ($this->cache->getByTag($node->getIdentifier()) as $value) {
+			list($uri, $zone) = Arrays::trimExplode('||', $value);
+			try {
+				$cacheDefinition = $this->cacheDefinitionFactory->create($zone);
+				$this->purgeCacheByRequestUri($uri, $cacheDefinition);
+			} catch (\Ttree\Cloudflare\Exception $exception) {
+				$this->systemLogger->log(sprintf("Unable to clear cache for \"%s\"", $uri), LOG_ERR, NULL, 'Cloudflare');
+			}
 		}
 	}
 
@@ -74,11 +100,14 @@ class RequestCacheService {
 	 * Purge Cloudflare cache by URI
 	 *
 	 * @param string $uri
-	 * @todo really purge CF cache
+	 * @param CacheDefinition $cacheDefinition
 	 */
-	public function purgeCacheByRequestUri($uri) {
+	public function purgeCacheByRequestUri($uri, CacheDefinition $cacheDefinition) {
 		$entryIdentifier = $this->createRequestUriCacheRecordIdentifier($uri);
 		$this->remove($entryIdentifier);
+		if ($cacheDefinition->getEnable() === TRUE) {
+			$this->apiService->purgeCacheByUri($uri, $cacheDefinition);
+		}
 		$this->systemLogger->log(sprintf("Clear Cloudflare cache for \"%s\"", $uri), LOG_INFO, NULL, 'Cloudflare');
 	}
 
@@ -86,9 +115,10 @@ class RequestCacheService {
 	 * Create a cache record for the given URI tagged by used Node identifier
 	 *
 	 * @param string $uri
+	 * @param CacheDefinition $cacheDefinition
 	 * @param array $nodes
 	 */
-	public function createRequestUriCacheRecord($uri, array $nodes) {
+	public function createRequestUriCacheRecord($uri, array $nodes, CacheDefinition $cacheDefinition) {
 		$this->removeRequestUriCacheRecord($uri);
 		$tags = array();
 		foreach ($nodes as $node) {
@@ -96,7 +126,7 @@ class RequestCacheService {
 			$identifier = $node->getIdentifier();
 			$tags[$identifier] = $identifier;
 		}
-		$this->set($uri, $tags);
+		$this->set($uri, $tags, $cacheDefinition);
 		$this->systemLogger->log('Create new request cache record', LOG_DEBUG, NULL, 'Cloudflare');
 	}
 
